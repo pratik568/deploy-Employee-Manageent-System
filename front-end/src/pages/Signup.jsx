10b8daa027ecba8b8bf13c7bd97b1950437a3b97
Pratik <94685060+pratik568@users.noreply.github.com>
2024-11-28 00:07:47 +0530

    Update Signup.jsx

--- front-end/src/pages/Signup.jsx
+++ front-end/src/pages/Signup.jsx
@@ -28,7 +28,7 @@ const Signup = () => {
       return handleError("name, email and password are required");
     }
     try {
-      const url = "http://localhost:8080/auth/signup";
+      const url = "https://deploy-employee-manageent-system.vercel.app/auth/signup";
       const response = await fetch(url, {
         method: "POST",
         headers: {
