a5498d0bd8e549aa50f15ee3953ce322ba6a95c8
Pratik <94685060+pratik568@users.noreply.github.com>
2024-11-28 00:09:22 +0530

    Update Home.jsx

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -46,7 +46,7 @@ const Home = () => {
   const handleAdminLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:8080/auth/admin/login", {
+      const response = await fetch("https://deploy-employee-manageent-system.vercel.app/auth/admin/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(adminCredentials),
